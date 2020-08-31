7b7870956fd778c874a91fe5bfbd93b21937a55c
Phil Austin <paustin@eos.ubc.ca>
2020-08-31 14:18:58 -0700

    remove _build

--- notebooks/_build/jupyter_execute/99-Appendix/99_02-ASCII-Character-Codes.py
+++
@@ -1,7 +0,0 @@
-# ASCII Character Codes
-
-A listing of ASCII character codes is below. ASCII character codes  character codes can be accessed using the following code:
-
-for n in range(32, 127):
-    print(f'ASCII code: {n}     Character: {chr(n)}')
-
