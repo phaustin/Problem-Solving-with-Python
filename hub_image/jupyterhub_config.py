dde1ecc91f9bb7e7584495d80983cac7485e1182
Phil Austin <you@example.comw>
2020-09-19 11:44:43 -0700

    update lock files, and hub_image

--- hub_image/jupyterhub_config.py
+++ hub_image/jupyterhub_config.py
@@ -8,6 +8,7 @@ c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.JupyterHub.hub_ip = '0.0.0.0'
 # the hostname/ip that should be used to connect to the hub
 # this is usually the hub container's name
+<<<<<<< HEAD
 c.JupyterHub.hub_connect_ip = 'book2_jupyterhub_1'
 
 # pick a docker image. This should have the same version of jupyterhub
@@ -20,6 +21,22 @@ c.DockerSpawner.network_name = 'book2_jupyterhub'
 #c.DockerSpawner.default_url = '/lab'
 c.DockerSpawner.notebook_dir = '~/notebooks'
 #c.DockerSpawner.args = ['--NotebookApp.default_url=/notebooks/index.ipynb']
+c.JupyterHub.hub_connect_ip = 'jupyterhub'
+
+# pick a docker image. This should have the same version of jupyterhub
+# in it as our Hub.
+c.DockerSpawner.image_whitelist = {'e350':'phaustin/notebook:0.1',
+                                   'a301':'phaustin/notebook:0.1'}
+# tell the user containers to connect to our docker network
+c.DockerSpawner.network_name = 'jupyterhub'
+#c.DockerSpawner.default_url = '/lab'
+c.DockerSpawner.notebook_dir = '~/notebooks'
+
+
+# explicitly set cmd, so we start jupyterhub-singleuser rather than jupyter notebook
+# This is useful when running docker images that aren't built specifically for JupyterHub
+c.DockerSpawner.cmd = 'jupyterhub-singleuser'
+
 
 # delete containers when the stop
 c.DockerSpawner.remove = True
